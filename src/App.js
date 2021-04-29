84c259910c49ff6185da445e4d7984e8817577d4
Santiagoalba <sanalba2222@gmail.com>
2021-04-28 21:47:09 -0300

    banner y header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import './App.css';
-import Header from './components/Header';
+import "./App.css";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Cart from "./components/Cart";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/cart">
+            <Cart />
+          </Route>
+
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/Cart.js
@@ -0,0 +1,8 @@
+import { styled } from "@material-ui/core";
+import React from "react";
+
+function Cart() {
+  return <div>Cart</div>;
+}
+
+export default Cart;
